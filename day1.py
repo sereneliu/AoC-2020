6f5aafaa095cd2aed0848dcbf268776230b88320
Serene Liu <github@serenely.me>
2020-12-07 21:25:12 -0500

    Day 1: brute force solution

--- day1.py
+++ day1.py
@@ -1,5 +1,13 @@
 puzzle_input = open('day1.txt', 'r').read().split('\n')
 
-# Part 1
+def fix_report(report):
+    for expense1 in report:
+        for expense2 in report:
+            for expense3 in report:
+                expense1 = int(expense1)
+                expense2 = int(expense2)
+                expense3 = int(expense3)
+                if expense1 + expense2 + expense3 == 2020:
+                    return expense1 * expense2 * expense3
 
-# Part 2
+print(fix_report(puzzle_input))
